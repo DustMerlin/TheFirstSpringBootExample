419042f04fc7205de93c0d37841e3fb562c4093d
Merlin Dust <1723978237@qq.com>
2020-01-18 10:35:45 +0800

    init repo

---
+++ src/main/java/com/Merlin/hello/controller/HelloController.java
@@ -0,0 +1,21 @@
+package com.Merlin.hello.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+//Created by conteoller on 2020.1.18
+
+@Controller
+public class HelloController {
+    //访问时不能出现空格http://localhost:8080/hello?name = Merlin   错误
+    //    http://localhost:8080/hello?name=Merlin                  正确
+    @GetMapping("/hello")
+    public String hello(@RequestParam(name = "name") String name, Model model){
+        model.addAttribute("name",name);
+        return "hello";
+
+    }
+
+}
